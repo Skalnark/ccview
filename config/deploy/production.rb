15b4e9e448db32c9f0633b88f4b881ad357bb41e
skalnark <skalnark@live.com>
2018-04-04 12:38:42 +0000

    production.rb changed, final address added on local project

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-role :app, %w{deploy@ec2-35-160-179-45.us-west-2.compute.amazonaws.com}
-role :web, %w{deploy@ec2-35-160-179-45.us-west-2.compute.amazonaws.com}
-role :db, %w{deploy@ec2-35-160-179-45.us-west-2.compute.amazonaws.com}
+role :app, %w{user@server.com}
+role :web, %w{user@server.com}
+role :db, %w{user@server.com}
