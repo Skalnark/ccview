1790f34352e85532fae0eb63121081d3057c33ae
skalnark <skalnark@live.com>
2018-12-02 19:05:50 -0300

    image changed

--- app/controllers/clinic_cases_controller.rb
+++ app/controllers/clinic_cases_controller.rb
@@ -72,6 +72,6 @@ class ClinicCasesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def clinic_case_params
-      params.require(:clinic_case).permit(:title, :clinicInformation, :description, :caseEvolution, :extraInformation, :term, images: [])
+      params.require(:clinic_case).permit(:title, :clinicInformation, :description, :caseEvolution, :extraInformation, :term, :images)
     end
 end
